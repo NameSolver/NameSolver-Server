e0a4359726731a9b86c1370e687b9cf21cb640d6
PH5555 <lmkn5342@gmail.com>
2025-03-07 15:36:13 +0900

    initial setting

---
+++ src/main/java/com/dongdong/nameSolver/NameSolverApplication.java
@@ -0,0 +1,13 @@
+package com.dongdong.nameSolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameSolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dongdong/nameSolver/NameSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongdong.nameSolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
