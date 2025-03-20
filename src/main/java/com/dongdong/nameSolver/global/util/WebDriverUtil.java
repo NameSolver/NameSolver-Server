30f1692a2b74b8030a4934daabdb65d3b1bd8788
김동현 <47862506+PH5555@users.noreply.github.com>
2025-03-20 17:48:02 +0900

    Feat: selenium 설정

---
+++ src/main/java/com/dongdong/nameSolver/global/util/WebDriverUtil.java
@@ -0,0 +1,44 @@
+package com.dongdong.nameSolver.global.util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+
+import java.time.Duration;
+
+@Component
+public class WebDriverUtil {
+    private final static String WEB_DRIVER_PATH = "C:\\Driver\\chromedriver-win64\\chromedriver-win64";
+
+    public static WebDriver getChromeDriver() {
+        if (ObjectUtils.isEmpty(System.getProperty("webdriver.chrome.driver"))) {
+            System.setProperty("webdriver.chrome.driver", WEB_DRIVER_PATH);
+        }
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--lang=ko");
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.setCapability("ignoreProtectedModeSettings", true);
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        return driver;
+    }
+
+    public static void quit(WebDriver driver) {
+        if (!ObjectUtils.isEmpty(driver)) {
+            driver.quit();
+        }
+    }
+
+    public static void close(WebDriver driver) {
+        if (!ObjectUtils.isEmpty(driver)) {
+            driver.close();
+        }
+    }
+
+}
