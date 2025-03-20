c598d44f805fc1d8012a4b0257ccde20c71cd2e8
PH5555 <lmkn5342@gmail.com>
2025-03-20 14:47:34 +0900

    chore: 기본 설정

---
+++ src/test/java/com/dongdong/nameSolver/auth/LoginServiceTest.java
@@ -0,0 +1,17 @@
+package com.dongdong.nameSolver.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LoginServiceTest {
+    @Test
+    void 인증문제출제(){
+
+    }
+
+    @Test
+    void 인증확인(){
+
+    }
+}
