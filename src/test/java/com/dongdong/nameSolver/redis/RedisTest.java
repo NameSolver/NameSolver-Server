adf7cf83bd135ad82c762191f90bc2931631390d
김동현 <47862506+PH5555@users.noreply.github.com>
2025-04-12 19:04:19 +0900

    test: 유효시간 테스트

--- src/test/java/com/dongdong/nameSolver/redis/RedisTest.java
+++ src/test/java/com/dongdong/nameSolver/redis/RedisTest.java
@@ -1,5 +1,6 @@
 package com.dongdong.nameSolver.redis;
 
+import com.dongdong.nameSolver.global.util.RedisUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +11,29 @@ import org.springframework.data.redis.core.ValueOperations;
 @SpringBootTest
 public class RedisTest {
     @Autowired
-    private RedisTemplate<String, String> redisTemplate;
+    private RedisUtil redisUtil;
 
     @Test
     void 레디스() {
-        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-
         String key = "name";
-        valueOperations.set(key, "giraffe");
-        String value = valueOperations.get(key);
-        Assertions.assertEquals(value, "giraffe");
+
+        redisUtil.setDate(key, "donghyun", 1000L);
+        String value = redisUtil.getDate(key);
+        Assertions.assertEquals(value, "donghyun");
+    }
+
+    @Test
+    void 레디스_유효시간() {
+        String key = "name";
+
+        redisUtil.setDate(key, "donghyun", 1000L);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String value = redisUtil.getDate(key);
+        Assertions.assertEquals(value, null);
     }
 }
