8e91d0405317d35e6752f1f245dd35d9f7c285c5
PH5555 <lmkn5342@gmail.com>
2025-04-29 14:06:05 +0900

    Feat: 토큰 발급 handler 구현

---
+++ src/main/java/com/dongdong/nameSolver/domain/auth/application/dto/TokenDto.java
@@ -0,0 +1,11 @@
+package com.dongdong.nameSolver.domain.auth.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TokenDto {
+    private String accessToken;
+    private String refreshToken;
+}
