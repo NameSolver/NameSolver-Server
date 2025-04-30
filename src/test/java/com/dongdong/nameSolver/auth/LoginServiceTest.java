291e7423781e56caea6f9482680f0282344e6ac7
김동현 <47862506+PH5555@users.noreply.github.com>
2025-04-30 10:03:13 +0900

    Test: 로그인 테스트 코드 수정

--- src/test/java/com/dongdong/nameSolver/auth/LoginServiceTest.java
+++ src/test/java/com/dongdong/nameSolver/auth/LoginServiceTest.java
@@ -7,11 +7,13 @@ import com.dongdong.nameSolver.domain.auth.application.dto.TokenDto;
 import com.dongdong.nameSolver.domain.auth.application.service.AuthService;
 import com.dongdong.nameSolver.domain.member.domain.entity.Member;
 import com.dongdong.nameSolver.domain.member.domain.repository.MemberRepository;
+import com.dongdong.nameSolver.global.jwt.JwtTokenProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import java.io.IOException;
 
@@ -22,6 +24,10 @@ public class LoginServiceTest {
     private AuthService loginService;
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtTokenProvider provider;
 
     @Test
     void 인증키_발급(){
@@ -43,36 +49,58 @@ public class LoginServiceTest {
     @Test
     @Transactional
     void 로그인_아이디_불일치() {
+        //회원 생성
+        SignUpDto signUpDto = new SignUpDto("김동현", "lmkn", "lmkn5342", "lmkn5342", "asdf");
+        String hashedPassword = passwordEncoder.encode(signUpDto.getPassword());
+        memberRepository.save(Member.join(signUpDto, hashedPassword));
+
         // 아이디, 비밀번호 가져오기
         SignInDto signInDto = new SignInDto();
         signInDto.setId("lmkn534");
         signInDto.setPassword("as");
 
-        TokenDto token = loginService.signIn(signInDto);
-        Assertions.assertThat(token.getAccessToken()).isNull();
+        Assertions.assertThatThrownBy(() -> loginService.signIn(signInDto))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("해당하는 아이디가 없습니다.");
     }
 
     @Test
     @Transactional
     void 로그인_아이디_일치_비번_불일치() {
+        //회원 생성
+        SignUpDto signUpDto = new SignUpDto("김동현", "lmkn", "lmkn5342", "lmkn5342", "asdf");
+        String hashedPassword = passwordEncoder.encode(signUpDto.getPassword());
+        memberRepository.save(Member.join(signUpDto, hashedPassword));
+
         // 아이디, 비밀번호 가져오기
         SignInDto signInDto = new SignInDto();
         signInDto.setId("lmkn5342");
         signInDto.setPassword("as");
 
-        TokenDto token = loginService.signIn(signInDto);
-        Assertions.assertThat(token.getAccessToken()).isNull();
+        Assertions.assertThatThrownBy(() -> loginService.signIn(signInDto))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("비밀번호가 불일치합니다.");
     }
 
     @Test
     @Transactional
     void 로그인_성공() {
+        //회원 생성
+        SignUpDto signUpDto = new SignUpDto("김동현", "lmkn", "lmkn5342", "lmkn5342", "asdf");
+        String hashedPassword = passwordEncoder.encode(signUpDto.getPassword());
+        Member save = memberRepository.save(Member.join(signUpDto, hashedPassword));
+
         // 아이디, 비밀번호 가져오기
         SignInDto signInDto = new SignInDto();
         signInDto.setId("lmkn5342");
         signInDto.setPassword("asdf");
 
+        // 토큰 반환 확인
         TokenDto token = loginService.signIn(signInDto);
         Assertions.assertThat(token.getAccessToken()).isNotNull();
+
+        // 토큰 정보 확인
+        String memberId = provider.extractSubject(token.getAccessToken());
+        Assertions.assertThat(memberId).isEqualTo(save.getMemberId().toString());
     }
 }
