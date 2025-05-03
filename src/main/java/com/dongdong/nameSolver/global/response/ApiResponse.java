a1c6251beb0659559e3bfe91d59369fd6dd7c0d0
김동현 <47862506+PH5555@users.noreply.github.com>
2025-05-03 21:48:39 +0900

    Style: api response 형식 수정

--- src/main/java/com/dongdong/nameSolver/global/response/ApiResponse.java
+++ src/main/java/com/dongdong/nameSolver/global/response/ApiResponse.java
@@ -1,14 +1,23 @@
 package com.dongdong.nameSolver.global.response;
 
+import lombok.Getter;
+
 import java.util.Map;
 
+@Getter
 public class ApiResponse<T> {
     private int code;
     private T data;
+    private String error;
 
     public ApiResponse(int code, T data) {
         this.code = code;
         this.data = data;
+    }
+
+    public ApiResponse(int code, String error) {
+        this.code = code;
+        this.error = error;
     }
 
     public static <T> ApiResponse<T> success(T data) {
@@ -16,6 +25,6 @@ public class ApiResponse<T> {
     }
 
     public static ApiResponse<Map<String, String>> error(int code, String message) {
-        return new ApiResponse<>(code, Map.of("error", message));
+        return new ApiResponse<>(code, message);
     }
 }
