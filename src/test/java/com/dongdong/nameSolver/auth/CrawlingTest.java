09b309695b682b8167a95f534fd3b33181cbba73
PH5555 <lmkn5342@gmail.com>
2025-05-03 15:26:34 +0900

    Test: 존재하지 않는 닉네임으로 크롤링 케이스 테스트

---
+++ src/test/java/com/dongdong/nameSolver/auth/CrawlingTest.java
@@ -0,0 +1,21 @@
+package com.dongdong.nameSolver.auth;
+
+import com.dongdong.nameSolver.domain.auth.application.service.AuthService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CrawlingTest {
+
+    @Autowired
+    private AuthService authService;
+
+    @Test
+    void 존재하지_않는_유저() {
+        Assertions.assertThatThrownBy(() -> {
+            authService.extractKey("fffee");
+        }).hasMessage("존재하지 않는 solvedAC닉네임입니다.");
+    }
+}
