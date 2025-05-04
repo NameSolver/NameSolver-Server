27ec0ee51d505b73494bb3e8f4c9ae7029c46d7d
PH5555 <lmkn5342@gmail.com>
2025-05-04 10:09:23 +0900

    Feat: 로그아웃 서비스 구현

--- src/main/java/com/dongdong/nameSolver/domain/member/application/service/MemberService.java
+++ src/main/java/com/dongdong/nameSolver/domain/member/application/service/MemberService.java
@@ -5,6 +5,7 @@ import com.dongdong.nameSolver.domain.member.domain.entity.Member;
 import com.dongdong.nameSolver.domain.member.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -14,8 +15,23 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    /**
+     * 멤버 정보 가져오는 메서드
+     */
     public MemberResponse findMember(UUID memberId) {
         Member member = memberRepository.findByMemberId(memberId).orElseThrow(() -> new RuntimeException("존재하지 않는 유저입니다."));
         return MemberResponse.from(member);
+    }
+
+    /**
+     * 로그아웃 메서드
+     */
+    @Transactional
+    public void signout(UUID memberId) {
+        // 멤버 가져오기
+        Member member = memberRepository.findByMemberId(memberId).orElseThrow(() -> new RuntimeException("존재하지 않는 유저입니다."));
+
+        // 리프레시 토큰 삭제
+        member.removeRefreshToken();
     }
 }
